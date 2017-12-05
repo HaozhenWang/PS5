05af19427b5917a41c24f65d00b30e2a3329a534
Haozhen <Haozhen@172.20.20.20>
2017-12-04 23:09:34 -0500

    Final

---
+++ src/main/java/pkgLibrary/Catalog.java
@@ -0,0 +1,104 @@
+package pkgLibrary;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+
+public class Catalog {
+
+	@XmlAttribute
+	int id;	
+	
+	@XmlElement(name="book")
+	ArrayList<Book> books;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public ArrayList<Book> getBooks() {
+		return books;
+	}
+	
+
+	public void setBooks(ArrayList<Book> books) {
+		this.books = books;
+	}
+	
+
+public void addBook(Book newBook) throws BookException {
+
+		
+		for (Book b: this.getBooks())
+		{
+			if (b.getId() == newBook.getId())
+			{
+				throw new BookException("Book already existed!");
+			}
+		}
+		
+		this.getBooks().add(newBook);
+		}
+
+	public static Catalog ReadXMLFile() {
+
+		Catalog cat = null;
+
+		String basePath = new File("").getAbsolutePath();
+		basePath = basePath + "//src//main//resources//XMLFiles//Books.xml";
+		File file = new File(basePath);
+
+		System.out.println(file.getAbsolutePath());
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			cat = (Catalog) jaxbUnmarshaller.unmarshal(file);
+			System.out.println(cat);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
+		return cat;
+
+	}
+	
+	public static void WriteXMLFile(Catalog cat) {
+		try {
+
+			String basePath = new File("").getAbsolutePath();
+			basePath = basePath + "//src//main//resources//XMLFiles//Books.xml";
+			File file = new File(basePath);
+
+			JAXBContext jaxbContext = JAXBContext.newInstance(Catalog.class);
+			Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+			// output pretty printed
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+			jaxbMarshaller.marshal(cat, file);
+			jaxbMarshaller.marshal(cat, System.out);
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	
+	
+	
+}
